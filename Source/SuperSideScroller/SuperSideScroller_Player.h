d3eecac48c2fbc22a4ad777f03c780251b522c67
Kirsten98 <42506533+Kirsten98@users.noreply.github.com>
2021-11-11 15:04:04 -0800

    Adds C++ Class to handle character max movement speed to control sprinting animations.

---
+++ Source/SuperSideScroller/SuperSideScroller_Player.h
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "SuperSideScrollerCharacter.h"
+#include "SuperSideScroller_Player.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SUPERSIDESCROLLER_API ASuperSideScroller_Player : public ASuperSideScrollerCharacter
+{
+	GENERATED_BODY()
+	
+
+public: 
+	ASuperSideScroller_Player();	
+
+protected:
+	//Override base character class function to setup our player input component
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	void Sprint();
+
+	void StopSprinting();
+
+private:
+	//Bool to control if we are sprinting. Failsafe
+	bool bIsSprinting;
+};
